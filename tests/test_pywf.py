23b91ea059cc5867adb1908092685adc4d5c2a4a
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-04-01 23:49:10 -0400

    Update test_pywf.py

--- tests/test_pywf.py
+++ tests/test_pywf.py
@@ -7,7 +7,13 @@ from pywf_open_source.paths import path_pyproject_toml
 
 def test():
     pywf = PyWf.from_pyproject_toml(path_pyproject_toml)
-    pywf.
+    pywf.create_virtualenv()
+    pywf.poetry_lock()
+    pywf.poetry_install_only_root()
+    pywf.poetry_install()
+    pywf.poetry_install_test()
+    pywf.run_unit_test()
+    pywf.run_cov_test()
 
 
 if __name__ == "__main__":
